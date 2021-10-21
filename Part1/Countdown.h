03f32f68680084c6bd3505e17991c686ccdacf35
Dylan Barker <dylan@dylantjb.com>
2021-10-21 17:17:30 +0100

    Attempt Part 1

--- Part1/Countdown.h
+++ Part1/Countdown.h
@@ -39,6 +39,83 @@ public:
 // Do not edit above this line
 
 // TODO: write code here:
+#include <vector>
+using namespace std;
+
+inline double evaluateCountdown(const string &expr) {
+  vector<double> numbers;
+
+  double op1;
+  double op2;
+
+  int pos = 0;
+  while (pos < expr.length()) {
+    switch (expr[pos]) {
+    case '+':
+      op1 = numbers.back();
+      numbers.pop_back();
+      op2 = numbers.back();
+      numbers.pop_back();
+
+      numbers.push_back(op2 + op1);
+      pos += 2;
+      break;
+    case '-':
+      op1 = numbers.back();
+      numbers.pop_back();
+      op2 = numbers.back();
+      numbers.pop_back();
+
+      numbers.push_back(op2 - op1);
+      pos += 2;
+      break;
+    case '/':
+      op1 = numbers.back();
+      numbers.pop_back();
+      op2 = numbers.back();
+      numbers.pop_back();
+
+      numbers.push_back(op2 / op1);
+      pos += 2;
+      break;
+    case '*':
+      op1 = numbers.back();
+      numbers.pop_back();
+      op2 = numbers.back();
+      numbers.pop_back();
+
+      numbers.push_back(op2 * op1);
+      pos += 2;
+      break;
+    default:
+      int i = 0;
+      while (expr[pos + i] != ' ') {
+        i++;
+      }
+      numbers.push_back(stod(expr.substr(pos, i)));
+      pos += i + 1;
+      break;
+    }
+  }
+  return numbers.back();
+}
+
+// Pick two numbers, use an operator, see if you get the (close to the) answer, pick another ... save the difference between it and the answer.
+// Keep all the differences with their expressions
+// Let it run 100 times then find the closest.
+inline CountdownSolution solveCountdownProblem(const vector<int> &numbers, const double target) {
+  vector<char> operators{'+', '-', '*', '/'};
+  string expression = "";
+  for (int i=0; i < numbers.size(); ++i) {
+    if (i > 1) {
+      char op = operators.back();
+      operators.pop_back();
+    } else {
+      expression += intToString(numbers.back()) + " ";
+    }
+  }
+  return CountdownSolution(expression);
+}
 
 
 // Do not edit below this line
--- Part1/CovidCase.h
+++ Part1/CovidCase.h
@@ -8,8 +8,55 @@ using std::ostream;
 
 
 
+#include <cmath>
+#include <string>
+using namespace std;
 
+inline const double convertToRadians(double coord);
 
+class CovidCase {
+private:
+  double lat;
+  double lon;
+  string name;
+  int age;
+  int time;
+
+public:
+  CovidCase(const double lat, const double lon, const string &name,
+            const int age, const int time)
+      : lat(), lon(), name(), age(), time() {}
+  void write(ostream &o) const {
+    o << '{' << lat << ", " << lon << ", " << '"' << name << "\", " << age
+      << ", " << time << '}' << "\n";
+  }
+  double getLon() const { return lon; }
+  double getLat() const { return lat; }
+  double distanceTo(const CovidCase &other) const {
+    // convert to radians (* pi/180)
+    double lat1 = convertToRadians(lat);
+    double lon1 = convertToRadians(lon);
+    double lat2 = convertToRadians(other.getLat());
+    double lon2 = convertToRadians(other.getLon());
+
+    double dlon = lon1 - lon2;
+    double dlat = lat1 - lat2;
+    double a = pow((sin(dlat / 2)), 2) +
+               cos(lat1) * cos(lat2) * pow((sin(dlon / 2)), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    return 3960 * c;
+  }
+};
+
+inline const double convertToRadians(double coord) {
+  return (atan(1) * coord) / 45;
+}
+
+inline ostream &operator<<(ostream &o, const CovidCase &rhs) {
+  rhs.write(o);
+  return o;
+}
 
 // don't write any code below this line
 
