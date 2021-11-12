1dd9a441d73aefdd60791f594b519c005fbacd02
Dylan Barker <dylan@dylantjb.com>
2021-11-12 15:20:55 +0000

    Attempt Part 5a

--- Part5/Sudoku.h
+++ Part5/Sudoku.h
@@ -4,8 +4,29 @@
 #include "Searchable.h"
 
 // TODO: Your Sudoku class goes here:
+#include <set>;
+using std::set;
 
+class Sudoku {
+private:
+  vector<vector<set<int>>> board;
+  int size;
 
+public:
+  Sudoku(int sizeIn) : size(sizeIn) {
+    for (auto &row : board) {
+      board.resize(sizeIn);
+      for (auto &col : row) {
+        for (int sqr = 1; sqr <= sizeIn; sqr++)
+          col.insert(sqr);
+      }
+    }
+  }
 
+  const int getSquare(int row, int col) const {
+    set<int> sqr = board[row][col];
+    return ((sqr.size() == 1) ? *(sqr.begin()) : -1);
+  }
+};
 
 #endif
